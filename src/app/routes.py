b4e7784c54eeceb779b6aa23a2aaa4ff42726f20
LLBueno <leandro.bueno@znc.com.br>
2020-05-15 22:18:43 -0300

    Configurado base template inicial

--- src/app/routes.py
+++ src/app/routes.py
@@ -2,7 +2,21 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Leandro'}
-    return render_template('index.html', title='Home', user=user)
+@app.route('/home')
+def home():
+    return render_template('home.html', title='home')
+
+
+@app.route('/imagem')
+def imagem():
+    return render_template('imagem.html', title='imagem')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', title='contato')
+
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html', title='sobre')
---
+++ src/app/static/js/base.js
@@ -0,0 +1,8 @@
+function getActiveMenu(){
+    let test = {{ title }};
+    let pagina = document.getElementById(test);
+    console.log(pagina);
+    console.log(test);
+}
+
+getActiveMenu()
